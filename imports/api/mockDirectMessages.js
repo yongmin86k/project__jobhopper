9a71f6d68efaa1d7485b485fcd4b919cc6e331d9
Yongmin Kim <yongmin86k@gmail.com>
2019-10-31 14:19:27 -0700

    Create mock data

---
+++ imports/api/mockDirectMessages.js
@@ -0,0 +1,16 @@
+[
+  {
+    _id: 1,
+    sender: 1,
+    receiver: 2,
+    contents: "Hello, David",
+    date: "2019-10-10 11:11:40"
+  },
+  {
+    _id: 1,
+    sender: 2,
+    receiver: 1,
+    contents: "Hello, Yongmin",
+    date: "2019-10-10 11:11:45"
+  }
+];
---
+++ imports/api/mockRate.js
@@ -0,0 +1,18 @@
+[
+  {
+    _id: 1,
+    type: "user",
+    userID: 1,
+    clientID: 2,
+    score: 5,
+    comments: null
+  },
+  {
+    _id: 2,
+    type: "client",
+    userID: 2,
+    clientID: 1,
+    score: 5,
+    comments: null
+  }
+];
