47e88f7da6817b1302d5938eeebb21b63fb80bd5
Yongmin Kim <yongmin86k@gmail.com>
2019-10-31 09:53:04 -0700

    Create the boilerplate with React

--- client/main.js
+++ client/main.js
@@ -1,22 +1,10 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import { Meteor } from "meteor/meteor";
 
-import './main.html';
+import React from "react";
+import ReactDOM from "react-dom";
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+import { Home } from "../imports/ui/pages";
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Meteor.startup(() => {
+  ReactDOM.render(<Home />, document.getElementById("root"));
 });
