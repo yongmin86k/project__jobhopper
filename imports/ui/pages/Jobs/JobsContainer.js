c5fce3d808abf87ddabff8186c94a23f809ee60c
Kanika Wadhawan <wadhawan.kanika@gmail.com>
2019-10-31 10:52:47 -0700

    Created Jobs under pages

---
+++ imports/ui/pages/Jobs/JobsContainer.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import Jobs from "./Jobs";
+
+class JobsContainer extends Component {
+  render() {
+    return <Jobs />;
+  }
+}
+export default JobsContainer;
