932ab3b7099f1c61329fe89ee8e124a5109261ce
Yongmin Kim <yongmin86k@gmail.com>
2019-10-31 11:06:50 -0700

    Import Page component

--- imports/ui/pages/Home/Home.js
+++ imports/ui/pages/Home/Home.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { Page } from "@material-ui/core";
 
 const Home = () => {
-  return <div>Hello world</div>;
+  return <Page>Hello world</Page>;
 };
 
 export default Home;
