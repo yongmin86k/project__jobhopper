3d0b38743fc11000594029bc256ea0bc920a7113
Yongmin Kim <yongmin86k@gmail.com>
2019-11-01 16:07:33 -0700

    Update global styles

--- client/main.js
+++ client/main.js
@@ -7,6 +7,8 @@ import Routes from "./src/Routes";
 
 import { MuiThemeProvider } from "@material-ui/core/styles";
 import theme from "./theme";
+import reset from "./reset.css";
+import styles from "./styles.css";
 
 Meteor.startup(() => {
   ReactDOM.render(
--- imports/ui/pages/Jobs/Jobs.js
+++ imports/ui/pages/Jobs/Jobs.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import { Fab } from "@material-ui/core";
+import styles from "./styles";
 
-const Jobs = () => {
-  return <div>Hey</div>;
+const Jobs = ({ classes }) => {
+  return (
+    <section id="filter-jobs" className="filterJobs">
+      <Fab
+        className={classes.filterBtn}
+        size="small"
+        variant="extended"
+        aria-label="like"
+        color="primary"
+      >
+        Category
+      </Fab>
+    </section>
+  );
 };
 
-export default Jobs;
+export default withStyles(styles)(Jobs);
