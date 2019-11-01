f017d0f2994c26e9d11fb5474cd726ae1780403f
Matt Waraich <mattsinghwaraich@gmail.com>
2019-11-01 10:42:03 -0700

    Routes added

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,13 +1,15 @@
-import React, { Fragment } from "react";
-import { Redirect, Route, Switch } from "react-router-dom";
+import React, { Fragment } from "./node_modules/react";
+import { Redirect, Route, Switch } from "./node_modules/react-router-dom";
 
-import { Home, Profile } from "/imports/ui/pages";
+import { Home, Profile, Jobs, Search } from "/imports/ui/pages";
 
 export default () => {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/profile" component={Profile} />
+      <Route exact path="/jobs" component={Jobs} />
+      <Route exact path="/search" component={Search} />
       <Redirect from="*" to="/" />
     </Switch>
   );
