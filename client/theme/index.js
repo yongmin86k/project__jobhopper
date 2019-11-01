34f28cbb02eba0475fac74891852cce4056adb97
Yongmin Kim <yongmin86k@gmail.com>
2019-11-01 15:41:36 -0700

    Create material-ui theme and set colors

---
+++ client/theme/index.js
@@ -0,0 +1,12 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+export default createMuiTheme({
+  palette: {
+    primary: {
+      main: "#EEEEEE"
+    },
+    secondary: {
+      main: "#2E7D32"
+    }
+  }
+});
