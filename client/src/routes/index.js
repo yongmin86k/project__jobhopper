5f8c3b390c8d258ac890e0cdc6c257539be6a7f2
Yongmin Kim <yongmin86k@gmail.com>
2019-11-01 20:29:35 -0700

    Create PrivateRoute

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -2,25 +2,25 @@ import React, { Fragment } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import { ViewerContext } from "/imports/ui/contexts/ViewerProvider";
 
+import { PrivateRoute } from "/imports/ui/components";
 import { Home, Profile, Jobs, Search } from "/imports/ui/pages";
 
 const routes = props => {
   return (
     <ViewerContext.Consumer>
       {({ currentUserId }) => {
-        return currentUserId ? (
+        return !currentUserId ? (
           <Switch>
-            <Route exact path="/profile" component={Profile} />
-            {/* <Route exact path="/jobs" component={Jobs} /> */}
-            <Route exact path="/search" component={Search} />
             <Route exact path="/" component={Home} />
             <Redirect from="*" to="/" />
           </Switch>
         ) : (
           <Switch>
-            <Route exact path="/jobs" component={Jobs} />
-            <Route exact path="/" component={Home} />
-            <Redirect from="*" to="/" />
+            <PrivateRoute exact path="/jobs" component={Jobs} />
+            <PrivateRoute exact path="/profile" component={Profile} />
+            <PrivateRoute exact path="/search" component={Search} />
+            <PrivateRoute exact path="/" component={Home} />
+            <Redirect from="*" to="/jobs" />
           </Switch>
         );
       }}
