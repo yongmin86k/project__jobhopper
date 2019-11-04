ecd827fefda374bf0068a7be185d8a116f99cf09
Yongmin Kim <yongmin86k@gmail.com>
2019-11-04 11:22:46 -0800

    Update styles separating background-images with other sections

--- imports/ui/pages/Post/PostContainer.js
+++ imports/ui/pages/Post/PostContainer.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
 import Post from "./Post";
+import { withStyles } from "@material-ui/core";
+import styles from "./styles";
 
 class PostContainer extends Component {
   render() {
-    return <Post />;
+    const { classes } = this.props;
+    return (
+      <section className="sectionWithMenuBar">
+        <Post />
+      </section>
+    );
   }
 }
-export default PostContainer;
+export default withStyles(styles)(PostContainer);
