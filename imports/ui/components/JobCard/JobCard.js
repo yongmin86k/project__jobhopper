d962b8bd1125d54265e18ebf7f68a8d3997dd4aa
Yongmin Kim <yongmin86k@gmail.com>
2019-11-08 10:11:38 -0800

    Update Countdown Timer for jobCard

--- imports/ui/components/JobCard/JobCard.js
+++ imports/ui/components/JobCard/JobCard.js
@@ -41,8 +41,14 @@ class JobCard extends Component {
     let currentTime = moment(),
       day = dateExpire.diff(currentTime, "days"),
       hour = dateExpire.diff(currentTime, "hours") % 24,
-      min = dateExpire.diff(currentTime, "minutes") % 60,
-      sec = dateExpire.diff(currentTime, "seconds") % 60,
+      min =
+        dateExpire.diff(currentTime, "minutes") % 60 < 10
+          ? "0" + (dateExpire.diff(currentTime, "minutes") % 60)
+          : dateExpire.diff(currentTime, "minutes") % 60,
+      sec =
+        dateExpire.diff(currentTime, "seconds") % 60 < 10
+          ? "0" + (dateExpire.diff(currentTime, "seconds") % 60)
+          : dateExpire.diff(currentTime, "seconds") % 60,
       remainTime =
         day > 1
           ? `${day} Days ${hour}:${min}:${sec}`
