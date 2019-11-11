2ed6f8e8d5e4ce829358ec794d1e47db6e0a91d6
Matt Waraich <mattsinghwaraich@gmail.com>
2019-11-11 10:49:59 -0800

    Single job page (stretch) edited + deleted some comments

---
+++ imports/ui/pages/SingleJob/SingleJob.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { JobCard } from "/imports/ui/components";
+
+const SingleJob = ({ previewValue }) => {
+  return <JobCard previewValue={previewValue} />;
+};
+
+export default SingleJob;
